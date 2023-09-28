932c2126e34ca2ca444efd48a04e11dfee0fabae
Tyler Marshall <60017345+tymarshall1@users.noreply.github.com>
2023-09-28 16:49:14 -0400

    made board update accordly to players turn

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ document
   .querySelector("#playervsPlayer")
   .addEventListener("click", () => DisplayController.showSelectionScreen());
 
-const gameBoard = (() => {
+const GameBoard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
+  let plays = 0;
 
   const getBoard = () => {
     return board;
@@ -12,9 +13,8 @@ const gameBoard = (() => {
   const markBoard = (player, index) => {
     if (!isTaken(index)) {
       board[index] = player.getPiece();
+      ++plays;
     }
-
-    //hasWon
   };
 
   const isTaken = (index) => {
@@ -24,11 +24,43 @@ const gameBoard = (() => {
     return false;
   };
 
-  const hasWon = () => {};
+  const determineTurn = () => {
+    return plays % 2 === 0 ? "player one" : "player two";
+  };
+
+  const resetBoard = () => {};
+
+  const checkForWinner = (peice) => {
+    //rows
+    if (board[0] === peice && board[1] === peice && board[2] === peice)
+      return `${peice} wins`;
+    if (board[3] === peice && board[4] === peice && board[5] === peice)
+      return `${peice} wins`;
+    if (board[6] === peice && board[7] === peice && board[8] === peice)
+      return `${peice} wins`;
+
+    //colls
+    if (board[0] === peice && board[3] === peice && board[6] === peice)
+      return `${peice} wins`;
+    if (board[1] === peice && board[4] === peice && board[7] === peice)
+      return `${peice} wins`;
+    if (board[2] === peice && board[5] === peice && board[8] === peice)
+      return `${peice} wins`;
+
+    //diagonals
+    if (board[0] === peice && board[4] === peice && board[8] === peice)
+      return `${peice} wins`;
+    if (board[2] === peice && board[4] === peice && board[6] === peice)
+      return `${peice} wins`;
+
+    return "no winner";
+  };
 
   return {
     markBoard,
     getBoard,
+    determineTurn,
+    checkForWinner,
   };
 })();
 
@@ -39,6 +71,7 @@ const DisplayController = (() => {
   let playerOne = null;
   let playerTwo = null;
 
+  //could just make this one function called swapScreens
   const clearScreen = (screen) => {
     screen.removeAttribute("class");
     screen.classList.add("hide-screen");
@@ -62,7 +95,10 @@ const DisplayController = (() => {
       setPlayers(e);
       showGameScreen();
     });
-    oIconBtn.addEventListener("click", (e) => setPlayers(e));
+    oIconBtn.addEventListener("click", (e) => {
+      setPlayers(e);
+      showGameScreen();
+    });
   };
 
   const showStartScreen = () => {
@@ -73,6 +109,40 @@ const DisplayController = (() => {
   const showGameScreen = () => {
     clearScreen(choosePieceScreen);
     addScreenClass(gameScreen, "game-screen");
+    let winner = "no winner";
+
+    const gridSquares = document.querySelectorAll(".grid-square");
+
+    gridSquares.forEach((square, index) => {
+      square.id = index;
+      square.addEventListener("click", () => {
+        const turn = GameBoard.determineTurn();
+        turn === "player one"
+          ? (GameBoard.markBoard(playerOne, square.id),
+            (winner = GameBoard.checkForWinner(playerOne.getPiece())))
+          : (GameBoard.markBoard(playerTwo, square.id),
+            (winner = GameBoard.checkForWinner(playerTwo.getPiece())));
+
+        addPieceToScreen(square, turn, winner, gridSquares);
+
+        console.log(winner);
+      });
+    });
+  };
+
+  const addPieceToScreen = (square, turn, winner, gridSquares) => {
+    square.textContent = GameBoard.getBoard()[square.id];
+    turn === "player one"
+      ? (square.style.backgroundColor = "red")
+      : (square.style.backgroundColor = "blue");
+
+    if (winner === "no winner") return;
+    else if (winner === "X wins" || winner === "O wins") {
+      gridSquares.forEach((indvSquare) => {
+        indvSquare.style.pointerEvents = "none";
+        indvSquare.style.opacity = 0.7;
+      });
+    }
   };
 
   const setPlayers = (e) => {
